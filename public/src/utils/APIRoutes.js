b302db534922dea2d35b5f65868ceb48106489c8
KashyapHindocha <k.hindocha777@gmail.com>
2022-09-08 11:14:25 +0530

    fixed host is not defined

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,7 +1,6 @@
-if(process.env.ENVIRONMENT === 'local'){
-  const host = "http://localhost:5000";
-}else if(process.env.ENVIRONMENT === 'production'){
-  const host = "https://quiet-caverns-00497.herokuapp.com";
+const host = "http://localhost:5000";
+if(process.env.ENVIRONMENT === 'production'){
+  host = "https://quiet-caverns-00497.herokuapp.com";
 }
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
