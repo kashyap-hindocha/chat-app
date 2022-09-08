5d7fbd5238291cd317a9ef6aca65115b26beffa5
KashyapHindocha <k.hindocha777@gmail.com>
2022-09-08 10:53:15 +0530

    added prod host

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,8 @@
-const host = "http://localhost:5000";
+if(process.env.ENVIRONMENT === 'local'){
+  const host = "http://localhost:5000";
+}else if(process.env.ENVIRONMENT === 'production'){
+  const host = "https://quiet-caverns-00497.herokuapp.com";
+}
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
