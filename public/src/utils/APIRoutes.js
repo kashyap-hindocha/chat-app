0994ee56e601cf007819898c77ea0f6c9592347a
KashyapHindocha <k.hindocha777@gmail.com>
2022-09-08 11:17:08 +0530

    force use prod env

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,7 +1,7 @@
-const host = "http://localhost:5000";
-if(process.env.ENVIRONMENT === 'production'){
-  host = "https://quiet-caverns-00497.herokuapp.com";
-}
+// const host = "http://localhost:5000";
+// if(process.env.ENVIRONMENT === 'production'){
+  ho1st = "https://quiet-caverns-00497.herokuapp.com";
+// }
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
