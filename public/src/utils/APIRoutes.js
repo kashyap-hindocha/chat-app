8895b07c6c643e6e2911d5f00f43da5ca8307bab
KashyapHindocha <k.hindocha777@gmail.com>
2022-09-08 11:19:00 +0530

    fixed typo

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,6 +1,6 @@
 // const host = "http://localhost:5000";
 // if(process.env.ENVIRONMENT === 'production'){
-  ho1st = "https://quiet-caverns-00497.herokuapp.com";
+const host = "https://quiet-caverns-00497.herokuapp.com";
 // }
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
